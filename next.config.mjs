b42c8da477b1e88d94a0098935eebd577438a2f7
Tanmay Surlekar <tanmay.s@octalogic.in>
2024-08-05 18:35:03 +0530

    refactor: set default route.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/hotels', // The default route you want to redirect to
+                permanent: true, // Set to true for a permanent redirect (308 status code), false for temporary (307 status code)
+            },
+        ];
+    },
+};
+
 
 export default nextConfig;
